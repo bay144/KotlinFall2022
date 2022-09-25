841b8a7a8298d1df0356d0d2a884338aa6d94598
Ana Osornio Baeza <bayleyosornio@gmail.com>
2022-09-25 10:58:55 -0500

    romanNumerals.kt

---
+++ src/main/kotlin/week03/romanNumerals.kt
@@ -0,0 +1,30 @@
+package week03
+fun main (args : Array<String>){
+print ("Write a number between 1 and 10: ")
+    val num = Integer.valueOf(readLine())
+
+if (num == 1) {
+  print ("I")
+} else if  (num == 2) {
+    print ("II")
+} else if  (num == 3) {
+  print ("III")
+} else if  (num == 4) {
+  print ("IV")
+} else if  (num == 5) {
+  print ("V")
+} else if  (num == 6) {
+  print ("VI")
+} else if  (num == 7) {
+  print ("VII")
+} else if  (num == 8) {
+  print ("VIII")
+} else if  (num == 9) {
+  print("IX")
+} else if (num == 10) {
+      print("X")
+} else { print("ERROR")}
+
+}
+
+
